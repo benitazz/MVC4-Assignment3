64795bc75998d2a7ab57d91c700776eb5f5166f8
unknown <bbdnet1087@benb.bbdnet.bbd.co.za>
2013-06-14 09:46:54 +0200

    initial commit

---
+++ Controllers/ManagementController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace Assignment.Areas.ContactDetails.Controllers
+{
+    public class ManagementController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }        
+
+        [HttpPost]
+        public ActionResult Index(Models.ContactDetails details)
+        {
+            return View(details);
+        }        
+    }
+}
